8b5ec2b6ca8f9c51c5ac2e3f187f41e2b001d347
nicolas-carolo <nicolas.carolo@outlook.com>
2018-03-13 16:12:21 +0100

    Carolo Nicolas

---
+++ src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
@@ -0,0 +1,25 @@
+/*
+    Carolo Nicolas  436266
+    Modica Camilla  439006
+    Novelli Giorgia 430088
+ */
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+
+public class CaroloNicolas extends Dealer {
+    @Override
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        if (roundNo % 2 == 0) {
+            if (getCoins() > 400){
+                return Briefcase.EMPTY;
+            } else {
+                return Briefcase.FULL;
+            }
+        } else {
+            return Briefcase.EMPTY;
+        }
+
+    }
+}
