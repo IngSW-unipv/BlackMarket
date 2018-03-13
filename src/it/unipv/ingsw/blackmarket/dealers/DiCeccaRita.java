df65707a64e2b3f1729a09a2238a58cd536dec48
Rita96 <rita.dicecca01@universitadipavia.it>
2018-03-13 16:04:04 +0100

    Prima prova funzionante

--- src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
+++ src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
@@ -6,7 +6,6 @@ import it.unipv.ingsw.blackmarket.Dealer;
 import java.util.Random;
 
 public class DiCeccaRita extends Dealer {
-    private Random randomGenerator = new Random();
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
         return Briefcase.EMPTY;
