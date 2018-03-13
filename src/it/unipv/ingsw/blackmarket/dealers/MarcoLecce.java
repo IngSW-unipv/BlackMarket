669f1cb25f91a54e52d641e7eafd1dbd765c66ed
marcolecce <lecciovich95@live.it>
2018-03-13 15:53:59 +0100

    1st commit. created MarcoLecce class that is a HonestDealer. Next Step: try to make it work.

---
+++ src/it/unipv/ingsw/blackmarket/dealers/MarcoLecce.java
@@ -0,0 +1,9 @@
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+
+public class MarcoLecce extends Dealer {
+    @Override
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) { return Briefcase.FULL; }
+}
