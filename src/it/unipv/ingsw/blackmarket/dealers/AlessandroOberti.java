8cecb4a35f958c2b916ceb9948a343fd5cd59276
ObeTheHurricane <37330345+ObeTheHurricane@users.noreply.github.com>
2018-03-13 16:26:02 +0100

    Add files via upload

---
+++ src/it/unipv/ingsw/blackmarket/dealers/AlessandroOberti.java
@@ -0,0 +1,16 @@
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+
+public class AlessandroOberti extends Dealer {
+
+    @Override
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        if(roundNo == 1) {
+            return Briefcase.FULL;
+        } else {
+            return Briefcase.EMPTY;
+        }
+    }
+}
