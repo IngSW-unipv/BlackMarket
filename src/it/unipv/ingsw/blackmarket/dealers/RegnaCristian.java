85bdb26406f054b9bb8a27d94c2f44043bde106a
Cristian Regna <cristian.regna01@universitadipavia.it>
2018-03-13 16:22:57 +0100

    First Commit

---
+++ src/it/unipv/ingsw/blackmarket/dealers/RegnaCristian.java
@@ -0,0 +1,25 @@
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+
+import java.util.Random;
+
+/*  Nome: Cristian Cognome: Regna
+*   Numero di matricola: 439009
+*   Strategia basata sul perdere al primo turno per dare un'ottima impressione, poi disonesto in modo tale da
+*   vincere contro il firstimpressionDealer
+* */
+
+public class RegnaCristian extends Dealer {
+
+    private Random randomGenerator = new Random();
+
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        if (roundNo == 1)
+            return Briefcase.FULL;
+        else
+            return Briefcase.EMPTY;
+
+    }
+}
