646f947e12aa28d7e3f6209bfcb2434e97cf92d0
jacopo96 <jacopo.ansaldi01@universitadipavia.it>
2018-03-13 16:27:52 +0100

    prova strategia base

--- src/it/unipv/ingsw/blackmarket/dealers/AnsaldiJacopo.java
+++ src/it/unipv/ingsw/blackmarket/dealers/AnsaldiJacopo.java
@@ -9,9 +9,22 @@ import java.util.ArrayList;
 public class AnsaldiJacopo extends Dealer {
     //ANSALDI 437472
     private ArrayList<Briefcase> previousCase;
+    private int numEmpty=0,numFull=0;
 
+    public AnsaldiJacopo(){
+        previousCase=new ArrayList<>();
+    }
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.EMPTY;
+       return Briefcase.EMPTY;
+    }
+
+    @Override
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        previousCase.add(exchange.secondBriefcase());
+        for(int i=0;i<previousCase.size();i++){
+            if(previousCase.get(i).equals(Briefcase.FULL)) numFull ++;
+            if(previousCase.get(i).equals(Briefcase.EMPTY)) numEmpty++;
+        }
     }
 }
