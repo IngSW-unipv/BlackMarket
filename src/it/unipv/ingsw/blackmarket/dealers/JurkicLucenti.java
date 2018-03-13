474cbc5f4a6f697c95d04efb048fb7cbd87beb82
mladen <mladen.jurkic01@universitadipavia.it>
2018-03-13 17:15:23 +0100

    testo

--- src/it/unipv/ingsw/blackmarket/dealers/JurkicLucenti.java
+++ src/it/unipv/ingsw/blackmarket/dealers/JurkicLucenti.java
@@ -2,27 +2,18 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
-import it.unipv.ingsw.blackmarket.Exchange;
+
+/*
+*
+* Jurkic Mladen, 434815
+* Lucenti Andrea, 439925
+*
+* */
 
 public class JurkicLucenti extends Dealer {
-    Briefcase case_;
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if (roundNo == 1)
-            return (Briefcase.FULL);
-        else {
-            return case_;
-        }
-    }
-
-    @Override
-    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
-
-        for(int i = 1; i <= totRounds; i++) {
-            if(i == (roundNo)){
-                case_ = exchange.secondBriefcase();
-            }
-        }
+        return Briefcase.EMPTY;
     }
 }
