e37c5e213cf624add98f48cd03c75c00dcb0bdff
Rita96 <rita.dicecca01@universitadipavia.it>
2018-03-13 16:04:18 +0100

    Prima prova funzionante

--- src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
+++ src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
@@ -3,7 +3,6 @@ package it.unipv.ingsw.blackmarket.dealers;
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
 
-import java.util.Random;
 
 public class DiCeccaRita extends Dealer {
     @Override
