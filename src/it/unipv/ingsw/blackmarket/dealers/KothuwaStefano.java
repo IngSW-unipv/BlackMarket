56f75056a38b54074b467f2f68241bf6ae0b78d6
Stefano <stefanokothuwa@live.it>
2018-03-13 15:47:34 +0100

    V1.1 ALWAYS RETURN BRIEFCASE.EMPTY

--- src/it/unipv/ingsw/blackmarket/dealers/KothuwaStefano.java
+++ src/it/unipv/ingsw/blackmarket/dealers/KothuwaStefano.java
@@ -7,6 +7,6 @@ import it.unipv.ingsw.blackmarket.Dealer;
 public class KothuwaStefano extends Dealer {
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.FULL;
+        return Briefcase.EMPTY;
     }
 }
