972136598e9128a58f386e455e5ee6a207b198ea
nicolas-carolo <nicolas.carolo@outlook.com>
2018-03-13 22:21:56 +0100

    Merge branch 'master' of https://github.com/IngSW-unipv/BlackMarket

--- src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
+++ src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
@@ -1,7 +1,4 @@
-/*
-    Carolo Nicolas  436266
-    Modica Camilla  439006
- */
+//Carolo Nicolas  436266
 package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
