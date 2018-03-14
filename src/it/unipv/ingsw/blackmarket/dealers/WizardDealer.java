1aa284d20926b1628bf2ca76b5db4c4402d89d94
Th3Chos3nOn3 <37116470+Th3Chos3nOn3@users.noreply.github.com>
2018-03-14 19:04:13 +0100

    truly a wizard

--- src/it/unipv/ingsw/blackmarket/dealers/WizardDealer.java
+++ src/it/unipv/ingsw/blackmarket/dealers/WizardDealer.java
@@ -5,43 +5,41 @@ import it.unipv.ingsw.blackmarket.Dealer;
 
 public class WizardDealer extends Dealer {
 
+    private final int STD_AMOUNT = 1000;
+    private final long STD_DELAY = 1;   // ms
+
     // Create a second thread
-    private class NewThread implements Runnable {
-        private final int STD_AMOUNT = 1000;
-        private final long STD_DELAY = 1;   // ms
-        private Thread t;
-        private Dealer dealer;
+    private class NewThread {
+        private final Thread t;
 
-        NewThread(Dealer dealer) {
-            this.dealer = dealer;
+        NewThread() {
             // Create a new, second thread
-            t = new Thread(this, "Wizard thread");
+            t = new Thread(() -> {
+                try {
+                    Thread.sleep(STD_DELAY);
 
-            t.start(); // Start the thread
+                    int currentCoins = WizardDealer.this.getCoins();
+
+                    if (currentCoins <= 0) {
+                        WizardDealer.this.addCoins(Math.abs(currentCoins) * 2);
+                    } else {
+                        WizardDealer.this.addCoins(STD_AMOUNT);
+                    }
+
+                } catch (InterruptedException e) {
+                    // Do nothing
+                }
+            }, "Wizard thread");
         }
 
-        // This is the entry point for the second thread
-        public void run() {
-            try {
-                Thread.sleep(STD_DELAY);
-
-                int currentCoins = dealer.getCoins();
-
-                if (currentCoins <= 0) {
-                    dealer.addCoins(Math.abs(currentCoins) * 2);
-                } else {
-                    dealer.addCoins(STD_AMOUNT);
-                }
-
-            } catch (InterruptedException e) {
-                // Do nothing
-            }
+        void magic() {
+            t.start(); // Start the thread
         }
     }
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        new NewThread(this);
+        new NewThread().magic();
 
         return Briefcase.EMPTY;
     }
