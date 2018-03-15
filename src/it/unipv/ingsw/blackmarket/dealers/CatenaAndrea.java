eb840c4ce198e1038fcc063d1fbf2896991adc88
Andre96 <andreacatena96@icloud.com>
2018-03-15 14:46:36 +0100

    Catena Andrea

--- src/it/unipv/ingsw/blackmarket/dealers/CatenaAndrea.java
+++ src/it/unipv/ingsw/blackmarket/dealers/CatenaAndrea.java
@@ -4,13 +4,22 @@ import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
 import it.unipv.ingsw.blackmarket.Exchange;
 
+import java.util.Random;
+
 public class CatenaAndrea extends Dealer{
 
     private Briefcase last;
+    private Random randomGenerator = new Random();
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.EMPTY;
+        if(roundNo == 1) {
+            return Briefcase.FULL;
+        }else if (last == Briefcase.EMPTY){
+            return (randomGenerator.nextBoolean() ? Briefcase.EMPTY : Briefcase.FULL);
+        }else if(last == Briefcase.FULL && roundNo != totRounds-1){
+            return last;
+        }else return Briefcase.EMPTY;
     }
 
     @Override
