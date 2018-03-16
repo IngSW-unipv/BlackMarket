644d82c4af7666a56a5ad69048788be43b52d80b
Matteo Goretti <teo@teogore.local>
2018-03-16 10:32:54 +0100

    goretti ritorno strategia semplice

--- src/it/unipv/ingsw/blackmarket/dealers/GorettiMatteo.java
+++ src/it/unipv/ingsw/blackmarket/dealers/GorettiMatteo.java
@@ -7,15 +7,19 @@ import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
 import it.unipv.ingsw.blackmarket.Exchange;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GorettiMatteo extends Dealer{
 
+  //  private boolean honest, cheater;              METTERE RICERCA PER GIOCATORI HONEST CHEATER ECC
+    private ArrayList<Briefcase> bags;
     private Briefcase briefcase;
     private double trust;
     private Random randomGenerator = new Random();
 
     public GorettiMatteo() {
+        this.bags = new ArrayList<Briefcase>();
         this.briefcase = Briefcase.EMPTY;
         this.trust = 0.5;
     }
@@ -23,25 +27,31 @@ public class GorettiMatteo extends Dealer{
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
 
-        if (roundNo == 1)
-            return (randomGenerator.nextBoolean() ? Briefcase.FULL : Briefcase.EMPTY);
-        else if(roundNo == totRounds)
+        if (roundNo == 1) {
+            bags.clear();           //a ogni nuovo dealer inizializzo strategia
+            trust = 0.5;
+            //return (randomGenerator.nextBoolean() ? Briefcase.FULL : Briefcase.EMPTY);
+            return Briefcase.FULL;
+        }else if((roundNo==3) || roundNo == totRounds){
             return Briefcase.EMPTY;
-        else
+        }
+        else {
             return briefcase;
-
+        }
     }
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
-        Briefcase bag1 = exchange.firstBriefcase();
-        Briefcase bag2 = exchange.secondBriefcase();
-        if(bag2 == Briefcase.EMPTY){
+        Briefcase bag = exchange.secondBriefcase();
+        bags.add(bag);
+
+        if(bag == Briefcase.EMPTY){
             trust -= 0.17;
         }else{
             trust += 0.1;
         }
         if(trust>=0.5) briefcase = Briefcase.FULL;
         else briefcase = Briefcase.EMPTY;
+
     }
 }
