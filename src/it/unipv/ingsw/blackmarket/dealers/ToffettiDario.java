9819950242b3c8bd1c2bbf59ee158cadc54944d6
Fenicottero <borawoiz@gmail.com>
2018-03-16 14:15:31 +0100

    add new dealer

--- src/it/unipv/ingsw/blackmarket/dealers/ToffettiDario.java
+++ src/it/unipv/ingsw/blackmarket/dealers/ToffettiDario.java
@@ -7,19 +7,30 @@ import it.unipv.ingsw.blackmarket.Exchange;
 import java.util.Random;
 
 public class ToffettiDario extends Dealer {
-    private Random randomGenerator = new Random();
-    Briefcase case_;
+    //private Random randomGenerator = new Random();
+    private boolean notCheated;
+    private int timesBeenHonest = 0;
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if (roundNo == 1)
-            return (randomGenerator.nextBoolean() ? Briefcase.FULL : Briefcase.EMPTY);
+        if (roundNo == 1) return Briefcase.FULL;
+        if (notCheated && timesBeenHonest <= 2 ) 
+            return Briefcase.FULL;
+
         else
-            return case_;
+            return Briefcase.EMPTY;
     }
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if(exchange.secondBriefcase() == Briefcase.EMPTY) {
+            notCheated = false;
+            timesBeenHonest = 0;
+        }
+        else {
+            notCheated = true;
+            timesBeenHonest++;
+        }
 
     }
 }
