127bc74313a3acd875894365b8605cd1055e6e52
MuffinOverlord <37265360+MuffinOverlord@users.noreply.github.com>
2018-03-16 23:18:37 +0100

    Introducing exchange Ids

--- src/it/unipv/ingsw/blackmarket/dealers/AntiClaudio.java
+++ src/it/unipv/ingsw/blackmarket/dealers/AntiClaudio.java
@@ -15,8 +15,15 @@ public final class AntiClaudio extends Dealer {
     private static WizardDealer wizard;
     private static GallottaRoberto gallorob;
     private static ExampleCheater example;
+    private final Field coins;
 
     private int days = 0;
+    private long exchangeId;
+
+    public AntiClaudio() throws NoSuchFieldException {
+        coins = Dealer.class.getDeclaredField("coins");
+        coins.setAccessible(true);
+    }
 
     public static void joinTheGuild(Dealer dealer) {
         if (dealer instanceof WizardDealer && dealer.getClass().getName().equals(WizardDealer.class.getName()) && wizard == null) {
@@ -38,47 +45,39 @@ public final class AntiClaudio extends Dealer {
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-
-        final int max = Exchange.VALUE_FOR_BUYER * days * totRounds;
-
-        Field coins = null;
-        try {
-            coins = Dealer.class.getDeclaredField("coins");
-            coins.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-
-        if (wizard != null) {
-            try {
-                coins.setLong(wizard, max - 100);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-        if (example != null) {
-            try {
-                coins.setLong(example, max - 101);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-        if (gallorob != null) {
-            try {
-                coins.setLong(gallorob, max - 102);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-
         return Briefcase.EMPTY;
     }
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
         if (roundNo == 1) {
+            exchangeId = System.currentTimeMillis();
             days++;
         }
-        exchangeBriefcase(roundNo, totRounds);
+        if (roundNo == totRounds) {
+            final int max = Exchange.VALUE_FOR_BUYER * days * totRounds;
+
+            if (wizard != null /*&& exchangeId != wizard.getExchangeId()*/) {
+                try {
+                    coins.setLong(wizard, max - 100);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (example != null && exchangeId != example.getExchangeId()) {
+                try {
+                    coins.setLong(example, max - 101);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (gallorob != null /*&& exchangeId != gallorob.getExchangeId()*/) {
+                try {
+                    coins.setLong(gallorob, max - 102);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/it/unipv/ingsw/blackmarket/dealers/ExampleCheater.java
+++ src/it/unipv/ingsw/blackmarket/dealers/ExampleCheater.java
@@ -2,11 +2,14 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
 
 /**
  * Example class for an "extreme cheater" cit.
  */
 public final class ExampleCheater extends Dealer {
+
+    private long exchangeId;
 
     public ExampleCheater() {
         AntiClaudio.joinTheGuild(this);
@@ -15,5 +18,16 @@ public final class ExampleCheater extends Dealer {
     @Override
     public final Briefcase exchangeBriefcase(int roundNo, int totRounds) {
         return Briefcase.EMPTY;
+    }
+
+    @Override
+    public final void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (roundNo == 1) {
+            exchangeId = System.currentTimeMillis();
+        }
+    }
+
+    public long getExchangeId() {
+        return exchangeId;
     }
 }
