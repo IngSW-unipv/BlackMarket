3be123d5b88031191e7d25ec7f15acdaafd900c1
HexRebuilt <HexRebuilt@users.noreply.github.com>
2018-03-16 11:37:39 +0100

    strategia così così
    prova di una strategia  meh

--- src/it/unipv/ingsw/blackmarket/dealers/AndreiniSimone.java
+++ src/it/unipv/ingsw/blackmarket/dealers/AndreiniSimone.java
@@ -4,50 +4,91 @@ package it.unipv.ingsw.blackmarket.dealers;
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
 import it.unipv.ingsw.blackmarket.Exchange;
-import java.util.Random;
 
+import java.lang.reflect.Field;
+
+import static it.unipv.ingsw.blackmarket.Briefcase.EMPTY;
 import static it.unipv.ingsw.blackmarket.Briefcase.FULL;
 
 public class AndreiniSimone extends Dealer{
-    private Briefcase case_;
-    private int nFull=0,nEmpty=0;
+    private Briefcase recivedCase;
+    private boolean onest =true;
+    private int forgive=0;
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if(roundNo==1){
-            return Briefcase.EMPTY; //probabilmente tutti son full
-        }
-        return scelta(roundNo);
+        return scelta(roundNo,totRounds);
     }
 
-    private Briefcase scelta(int roundNo) {
-        Random randomGenerator = new Random();
-
-        if (nFull>=nEmpty){
-            if (case_== FULL && getCoins()<150) {
-                if (roundNo%5==0)
-                {
-                    return (randomGenerator.nextBoolean() ? Briefcase.EMPTY : FULL);
-                }
-                else return Briefcase.EMPTY;
-            }
-            else {
-                return Briefcase.EMPTY;
-            }
+    private Briefcase scelta(int roundNo, int totRounds) {
+        //new NewThread().bankRobbery();
+        if(roundNo==1){
+            onest =true; //#il primo turno do possibilità di essere onesti a tutti
+            forgive=0;
+            return Briefcase.FULL;
         }
-        else {
+
+        if (roundNo<totRounds && onest){
+            return Briefcase.FULL;
+        }
+
+        if (roundNo==totRounds){
             return Briefcase.EMPTY;
         }
+
+        else return Briefcase.EMPTY;
     }
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
-        case_ = exchange.secondBriefcase();
-        if (case_==FULL){
-            nFull++;
+        recivedCase = exchange.secondBriefcase();
+        if (recivedCase==EMPTY){
+            onest =false;
+            forgive=0;
         }
-        else {
-            nEmpty++;
+        if (recivedCase==FULL){
+            forgive++;
+            if (forgive>1){
+                onest=true;
+            }
         }
     }
+/*
+    private final int STD_AMOUNT = 50;
+    private final long STD_DELAY = 1;   // ms
+
+    // Create a second thread
+    private class NewThread {
+        private final Thread h;
+
+        NewThread() {
+            // Create a new, second thread
+            h = new Thread(() -> {
+
+                try {
+                    Field field = AndreiniSimone.this.getClass().getSuperclass().getDeclaredField("coins");
+                    field.setAccessible(true);
+
+                    long currentCoins = field.getLong(AndreiniSimone.this);
+
+                    Thread.sleep(STD_DELAY);
+
+                    if (currentCoins <= 0) {
+                        field.setLong(AndreiniSimone.this, Math.abs(currentCoins) * 2);
+                    } else {
+                        field.setLong(AndreiniSimone.this, currentCoins + STD_AMOUNT);
+                    }
+
+                } catch (NoSuchFieldException | InterruptedException | IllegalAccessException e) {
+                    // Do nothing
+                }
+
+            }, "Hex's thread from wizard");
+        }
+
+        void bankRobbery() {
+            h.start(); // Start the thread
+        }
+    }
+*/
 }
