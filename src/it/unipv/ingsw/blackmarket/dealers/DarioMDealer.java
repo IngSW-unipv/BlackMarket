3e34bf471e1f9b2de717f17dfac5576715aea0a9
Dareus1996 <32876188+Dareus1996@users.noreply.github.com>
2018-03-17 20:40:17 +0100

    Ok, now Cheater don't win so much.
    Need to do some labor limae

--- src/it/unipv/ingsw/blackmarket/dealers/DarioMDealer.java
+++ src/it/unipv/ingsw/blackmarket/dealers/DarioMDealer.java
@@ -7,6 +7,9 @@ import it.unipv.ingsw.blackmarket.Exchange;
 public class DarioMDealer extends Dealer{
 
     private Briefcase briefcase;
+    private Briefcase briefcaseRound1;
+    private Briefcase briefcaseRound2;
+    private boolean tooGreedy = false;
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
@@ -21,21 +24,46 @@ public class DarioMDealer extends Dealer{
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
 
-            // Provo a fregarlo se é un Majority
-            if (roundNo == totRounds / 2) {
-                briefcase = Briefcase.EMPTY;
-            }
-            // Mi metto subito al riparo nel caso sia un TitForTat
-            else if (roundNo == (totRounds / 2) + 1) {
-                briefcase = Briefcase.FULL;
-            }
-            // Capisco se é un TitForThat o un Majority
-            else if (roundNo == ((totRounds / 2) + 2) && exchange.secondBriefcase() == Briefcase.FULL) {
-                briefcase = Briefcase.EMPTY;
-            }
-            // Frego il TitForTat
-            else if (roundNo == totRounds - 1) {
-                briefcase = Briefcase.EMPTY;
-            }
+        if(roundNo == 1){
+            briefcaseRound1 = exchange.secondBriefcase();
+        }
+
+        if (roundNo == 2){
+            briefcaseRound2 = exchange.secondBriefcase();
+        }
+
+        if(roundNo == 2 && exchange.secondBriefcase() == Briefcase.EMPTY && briefcaseRound1 == Briefcase.EMPTY){
+            briefcase = Briefcase.EMPTY;
+            tooGreedy = true;
+        }
+
+        // Segnalo quando sto giocando contro qualcuno troppo greedy
+        if(roundNo == 3 && exchange.secondBriefcase() == Briefcase.EMPTY && (briefcaseRound1 == Briefcase.EMPTY || briefcaseRound2 == Briefcase.EMPTY)){
+            tooGreedy =true;
+            briefcase = Briefcase.EMPTY;
+        }
+
+        // Provo a fregarlo se é un Majority
+        if (roundNo == (totRounds / 2)) {
+            briefcase = Briefcase.EMPTY;
+        }
+
+        // Mi metto subito al riparo nel caso sia un TitForTat
+        if (roundNo == (totRounds / 2) + 1 && !tooGreedy) {
+            briefcase = Briefcase.FULL;
+        } else if(roundNo == (totRounds / 2) + 1 && tooGreedy){
+            briefcase = Briefcase.EMPTY;
+        }
+
+        // Capisco se é un TitForThat o un Majority
+        if (roundNo == ((totRounds / 2) + 2) && exchange.secondBriefcase() == Briefcase.FULL) {
+            briefcase = Briefcase.EMPTY;
+        }
+
+        // Frego il TitForTat
+        if (roundNo == totRounds - 1) {
+            briefcase = Briefcase.EMPTY;
+            tooGreedy = false;
+        }
     }
 }
