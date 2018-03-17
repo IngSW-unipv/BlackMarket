5f6a6eb3a9851cca37019ed23b91d332159f65bf
AlbertoGit30 <albertoschillaci30@gmail.com>
2018-03-17 14:35:56 +0100

    Primo commit

--- src/it/unipv/ingsw/blackmarket/dealers/SchillaciIsHonest.java
+++ src/it/unipv/ingsw/blackmarket/dealers/SchillaciIsHonest.java
@@ -3,14 +3,10 @@ package it.unipv.ingsw.blackmarket.dealers;
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
 
-public class Curcio extends Dealer {
+public class SchillaciIsHonest extends Dealer{
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if (roundNo == 1) {
-            return (Briefcase.FULL);
-        } else
-            return Briefcase.EMPTY;
+        return Briefcase.FULL;
     }
-
 }
