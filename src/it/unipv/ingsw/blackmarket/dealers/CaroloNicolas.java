75cf4b2039fa355dfe714df4136d149f3014515a
nicolas-carolo <nicolas.carolo@outlook.com>
2018-03-18 18:12:56 +0100

    Carolo Nicolas v.2

--- src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
+++ src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
@@ -3,10 +3,31 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
 
 public class CaroloNicolas extends Dealer {
+    private Briefcase bag = Briefcase.FULL;
+    private boolean isReliable = true;
+
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.EMPTY;
+        if (roundNo == 1){
+            bag = Briefcase.FULL;
+            isReliable = true;
+        }
+        if (roundNo > totRounds / 2){
+            return Briefcase.EMPTY;
+        } else {
+            return bag;
+        }
+    }
+
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (exchange.secondBriefcase().equals(Briefcase.FULL)){
+            bag = Briefcase.FULL;
+        } else{
+            bag = Briefcase.EMPTY;
+            isReliable = false;
+        }
     }
 }
