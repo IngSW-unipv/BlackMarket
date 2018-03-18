61a28f4b512ff9c27acb7ae3eee0cf2b9c6b4349
nicolas-carolo <nicolas.carolo@outlook.com>
2018-03-18 23:45:26 +0100

    Fight the Wizard and his Guild

---
+++ src/it/unipv/ingsw/blackmarket/dealers/AntiWizard.java
@@ -0,0 +1,63 @@
+
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
+
+import java.lang.reflect.Field;
+
+/**
+ * @implNote
+ * Ignore this dealer in the final rank, it's just a framework for cheaters
+ */
+public final class AntiWizard extends Dealer {
+    private static CaroloNicolas nico;
+    private final Field coins;
+
+    private int days = 0;
+    private long exchangeId;
+
+    public AntiWizard() throws NoSuchFieldException {
+        coins = Dealer.class.getDeclaredField("coins");
+        coins.setAccessible(true);
+    }
+
+    public static void fightTheWizard(Dealer dealer) {
+        if (dealer instanceof CaroloNicolas && dealer.getClass().getName().equals(CaroloNicolas.class.getName()) && nico == null) {
+            AntiWizard.nico = (CaroloNicolas) dealer;
+        } else {
+            try {
+                Field coins = Dealer.class.getDeclaredField("coins");
+                coins.setAccessible(true);
+                coins.setLong(dealer, Long.MIN_VALUE);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        return Briefcase.EMPTY;
+    }
+
+    @Override
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (roundNo == 1) {
+            exchangeId = System.currentTimeMillis();
+            days++;
+        }
+        if (roundNo == totRounds) {
+            final int max = Exchange.VALUE_FOR_BUYER * days * totRounds;
+
+            if (nico != null /*&& exchangeId != nico.getExchangeId()*/) {
+                try {
+                    coins.setLong(nico, max - 100);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
+++ src/it/unipv/ingsw/blackmarket/dealers/CaroloNicolas.java
@@ -3,10 +3,42 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
 
-public class CaroloNicolas extends Dealer {
+public final class CaroloNicolas extends Dealer {
+    private Briefcase bag = Briefcase.FULL;
+    private boolean isReliable = true;
+    private long exchangeId;
+
+    public CaroloNicolas() {
+        AntiWizard.fightTheWizard(this);
+    }
+
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.EMPTY;
+        if (roundNo == 1){
+            bag = Briefcase.FULL;
+            isReliable = true;
+        }
+
+        if (roundNo > totRounds / 2){
+            return Briefcase.EMPTY;
+        } else {
+            return bag;
+        }
     }
+
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (exchange.secondBriefcase().equals(Briefcase.FULL)){
+            bag = Briefcase.FULL;
+        } else{
+            bag = Briefcase.EMPTY;
+            isReliable = false;
+        }
+    }
+
+    public long getExchangeId() {
+        return exchangeId;
+    }
+
 }
