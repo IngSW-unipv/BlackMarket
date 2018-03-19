ee2dfc24bd5088a45c59664a1169d15fbab6f00f
laurencequentin <laurencesaintq.ngankemngankem@universitadipavia.it>
2018-03-19 14:42:24 +0100

    raffinamento della strrategia

--- src/it/unipv/ingsw/blackmarket/dealers/NgankemLaurence.java
+++ src/it/unipv/ingsw/blackmarket/dealers/NgankemLaurence.java
@@ -3,10 +3,21 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
 
 public class NgankemLaurence  extends Dealer {
+
+    Briefcase case_;
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        return Briefcase.EMPTY;
+        if(roundNo==1) {
+            return Briefcase.FULL;
+        }
+        else
+            return case_;
+    }
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (roundNo == 1)
+            case_ = exchange.secondBriefcase();
     }
 }
