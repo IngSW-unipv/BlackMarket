525cbde044b554db192d71fed7b6bd9077fc9c92
tudor391 <tudor.94@hotmail.it>
2018-03-19 22:37:58 +0100

    Want some fungus?

---
+++ src/it/unipv/ingsw/blackmarket/dealers/BordeaTudor.java
@@ -0,0 +1,16 @@
+package it.unipv.ingsw.blackmarket.dealers;
+        import it.unipv.ingsw.blackmarket.Briefcase;
+        import it.unipv.ingsw.blackmarket.Dealer;
+        import it.unipv.ingsw.blackmarket.Exchange;
+
+public class BordeaTudor extends Dealer {
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        if((roundNo<2)&&(roundNo%2==2)){
+            return Briefcase.FULL;
+        }
+        else {
+            return Briefcase.EMPTY;
+        }
+    }
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) { }
+}
