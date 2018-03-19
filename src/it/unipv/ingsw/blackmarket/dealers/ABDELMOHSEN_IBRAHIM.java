057db92f3d8293b1ff9183d7df2d327539021df3
alaaeldin95 <37330751+alaaeldin95@users.noreply.github.com>
2018-03-19 14:05:49 +0100

    1.1

--- src/it/unipv/ingsw/blackmarket/dealers/ABDELMOHSEN_IBRAHIM.java
+++ src/it/unipv/ingsw/blackmarket/dealers/ABDELMOHSEN_IBRAHIM.java
@@ -2,7 +2,7 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
-
+/*ABDEL MOHSEN ALAA EL DIN*/
 public class ABDELMOHSEN_IBRAHIM extends Dealer {
  Briefcase briefcase;
     @Override
