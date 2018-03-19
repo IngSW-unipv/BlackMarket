335a6940e710b67c5e8f1b26afb4d47c342018da
VinCur <vincur96@gmail.com>
2018-03-19 23:31:09 +0100

    First Commit

--- src/it/unipv/ingsw/blackmarket/dealers/Curcio.java
+++ src/it/unipv/ingsw/blackmarket/dealers/Curcio.java
@@ -2,12 +2,27 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
 
 public class Curcio extends Dealer {
 
+    private Briefcase last;
+
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if(roundNo == 1) return Briefcase.FULL;
-        else return Briefcase.EMPTY;
+        if (roundNo == 1)
+            return Briefcase.FULL;
+        else
+            return last;
+    }
+
+    @Override
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if(roundNo<=5) {
+            last = exchange.secondBriefcase();
+        }
+        else {
+            last=Briefcase.EMPTY;
+        }
     }
 }
