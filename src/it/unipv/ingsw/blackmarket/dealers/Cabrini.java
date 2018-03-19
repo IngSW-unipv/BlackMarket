c3837ad6b49a3e0655bb1ee9ada6efd2d5657c72
AlexCabrini <37148134+AlexCabrini@users.noreply.github.com>
2018-03-19 22:52:52 +0100

    final version

--- src/it/unipv/ingsw/blackmarket/dealers/Cabrini.java
+++ src/it/unipv/ingsw/blackmarket/dealers/Cabrini.java
@@ -7,19 +7,27 @@ import it.unipv.ingsw.blackmarket.Exchange;
 
 public class Cabrini extends Dealer {
     Briefcase case_;
+    Boolean controllo = false;
 
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+
         if(roundNo == 1)
-            return Briefcase.EMPTY;
+            return Briefcase.FULL;
+        else if (controllo == true)
+            return case_;
         else
             return case_;
+
     }
+
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
-        if(roundNo > 5 )
-            case_ = exchange.secondBriefcase();
+        if(exchange.secondBriefcase().equals(Briefcase.EMPTY) ) {
+            controllo = true;
+            case_ = Briefcase.EMPTY;
+        }
         else
             case_ = Briefcase.EMPTY;
     }
