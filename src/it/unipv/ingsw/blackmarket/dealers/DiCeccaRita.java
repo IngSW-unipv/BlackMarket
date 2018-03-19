33f433d39c2d971159ab2eefe66b591936fcdb7f
Rita <rita.dicecca01@universitadipavia.it>
2018-03-19 22:25:14 +0100

    Final Version

--- src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
+++ src/it/unipv/ingsw/blackmarket/dealers/DiCeccaRita.java
@@ -7,28 +7,47 @@ import it.unipv.ingsw.blackmarket.Exchange;
 
 public class DiCeccaRita extends Dealer {
     Briefcase case_;
+    Briefcase result = null;
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
-        if (roundNo == 1) return Briefcase.EMPTY;
-            else
-            return case_;
+        if (roundNo == 1) case_ = Briefcase.FULL;
+        else {
+            if (roundNo > (totRounds/2))
+                case_ = Briefcase.EMPTY;
+        }
+        return case_;
     }
 
     @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
-        Briefcase result = Briefcase.EMPTY;
-
-        if ((roundNo == 1) || (roundNo == 2)) {
-            case_ = Briefcase.EMPTY;
-            result = exchange.secondBriefcase();
+        if (roundNo == 1) {
+            if (exchange.secondBriefcase() == Briefcase.FULL) {
+                case_ = Briefcase.FULL;
+                result = exchange.secondBriefcase();
+            }
+            if (exchange.secondBriefcase() == Briefcase.EMPTY) {
+                case_ = Briefcase.EMPTY;
+                result = exchange.secondBriefcase();
+            }
         }
-        if (roundNo >= 3) {
-            if ((result == Briefcase.EMPTY) || (exchange.secondBriefcase() == Briefcase.EMPTY)){
+
+        if (totRounds == roundNo) {
+            case_ = Briefcase.EMPTY;
+        }
+
+        if ((roundNo >=2) && (roundNo < totRounds)){
+            if ((result == Briefcase.EMPTY) && (exchange.secondBriefcase() == Briefcase.EMPTY)) {
                 case_ = Briefcase.EMPTY;
             }
-            else
+            if ((result == Briefcase.FULL) &&(exchange.secondBriefcase() == Briefcase.FULL)) {
                 case_ = Briefcase.FULL;
-
+            }
+            if ((result == Briefcase.EMPTY) && (exchange.secondBriefcase() == Briefcase.FULL)) {
+                case_ = Briefcase.FULL;
+            }
+            if ((result== Briefcase.FULL) && (exchange.secondBriefcase() == Briefcase.EMPTY)) {
+                case_ = Briefcase.EMPTY;
+            }
         }
     }
 }
