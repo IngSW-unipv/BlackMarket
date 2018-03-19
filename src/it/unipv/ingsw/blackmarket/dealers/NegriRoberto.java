046b84126681a3287ed2fd0817c7b6ea781079f2
h0000ly <37170577+h0000ly@users.noreply.github.com>
2018-03-19 15:11:07 +0100

    we uagliò bell stu codic

---
+++ src/it/unipv/ingsw/blackmarket/dealers/NegriRoberto.java
@@ -0,0 +1,24 @@
+package it.unipv.ingsw.blackmarket.dealers;
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
+
+public class NegriRoberto extends Dealer {
+
+
+
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+       if((roundNo<2)&&(roundNo%2==0)){
+           return Briefcase.FULL;
+       }
+       else {
+           return Briefcase.EMPTY;
+       }
+    }
+
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+
+    }
+
+
+}
