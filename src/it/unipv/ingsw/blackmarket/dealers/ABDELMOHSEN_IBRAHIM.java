edb6f7069149f61993cd3570e09bdc886d11c159
alaaeldin95 <37330751+alaaeldin95@users.noreply.github.com>
2018-03-19 15:38:36 +0100

    1.2

--- src/it/unipv/ingsw/blackmarket/dealers/ABDELMOHSEN_IBRAHIM.java
+++ src/it/unipv/ingsw/blackmarket/dealers/ABDELMOHSEN_IBRAHIM.java
@@ -2,18 +2,29 @@ package it.unipv.ingsw.blackmarket.dealers;
 
 import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
+
 /*ABDEL MOHSEN ALAA EL DIN*/
 public class ABDELMOHSEN_IBRAHIM extends Dealer {
- Briefcase briefcase;
+    private Briefcase bag = Briefcase.FULL;
+
     @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+        if (roundNo == 1){
+            bag = Briefcase.FULL;
+        }
+        if (roundNo > (totRounds / 2) + 1){
+            return Briefcase.EMPTY;
+        } else
+            return bag;
 
-        if (roundNo==1 )
-        {
-            return Briefcase.FULL;
+    }
 
-    }else {
-        return briefcase;
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if (exchange.secondBriefcase().equals(Briefcase.FULL)){
+            bag = Briefcase.EMPTY;
+        } else if (exchange.firstBriefcase().equals(Briefcase.FULL)){
+            bag = Briefcase.EMPTY;
         }
     }
 }
