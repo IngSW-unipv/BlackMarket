29b4da93644322bc2fbc88d5f229bb3115f8ab20
Daniele Ligato <daniele96ligato@gmail.com>
2018-03-19 23:41:42 +0100

    Ultima versione

---
+++ src/it/unipv/ingsw/blackmarket/dealers/DanieleLigato.java
@@ -0,0 +1,30 @@
+package it.unipv.ingsw.blackmarket.dealers;
+
+import it.unipv.ingsw.blackmarket.Briefcase;
+import it.unipv.ingsw.blackmarket.Dealer;
+import it.unipv.ingsw.blackmarket.Exchange;
+
+public class DanieleLigato extends Dealer {
+    
+//daniele ligato..
+
+    private Briefcase last;
+
+    public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
+
+        if (roundNo == 1)
+            return Briefcase.FULL;
+        else
+            return last;
+    }
+
+    @Override
+    public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
+        if(roundNo<=5) {
+            last = exchange.secondBriefcase();
+        }
+        else {
+            last=Briefcase.EMPTY;
+        }
+    }
+}
