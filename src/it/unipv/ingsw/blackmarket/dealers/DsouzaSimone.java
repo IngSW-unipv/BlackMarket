63e9e1ea1cc5ae84505cb9dd14fbb7675fb6a3b4
simonedsouza <simone.dsouza01@universitadipavia.it>
2018-03-19 12:51:14 +0100

    Matricola: 439380 V1

--- src/it/unipv/ingsw/blackmarket/dealers/DsouzaSimone.java
+++ src/it/unipv/ingsw/blackmarket/dealers/DsouzaSimone.java
@@ -4,10 +4,10 @@ import it.unipv.ingsw.blackmarket.Briefcase;
 import it.unipv.ingsw.blackmarket.Exchange;
 import it.unipv.ingsw.blackmarket.Dealer;
 
-public class DsouzaSimone {
+public class DsouzaSimone extends Dealer{
     Briefcase case_;
 
-
+    @Override
     public Briefcase exchangeBriefcase(int roundNo, int totRounds) {
 
         if(roundNo == 1) {
@@ -21,7 +21,7 @@ public class DsouzaSimone {
         }
     }
 
-
+    @Override
     public void exchangeResult(Exchange exchange, int roundNo, int totRounds) {
         if(roundNo==2)
             case_ = exchange.secondBriefcase();
